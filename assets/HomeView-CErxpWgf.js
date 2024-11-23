fcff4824cffc768b53ba9bc2723e56d32d19b8ba
Yordan Alipiev <jordanalipiev@yahoo.com>
2024-11-23 10:07:51 +0100

    Updates

---
+++ assets/HomeView-CErxpWgf.js
@@ -0,0 +1,1 @@
+import{_ as s,c as o,a as t,o as a}from"./index-DVHrPsnP.js";const c={},r={class:"about"};function n(_,e){return a(),o("div",r,e[0]||(e[0]=[t("h1",{class:"text-red-500"},"This is an home page",-1)]))}const d=s(c,[["render",n]]);export{d as default};
