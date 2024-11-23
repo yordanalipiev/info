21ea393d51dff42709ed8beb33d91333661a97a2
Yordan Alipiev <jordanalipiev@yahoo.com>
2024-11-23 12:56:48 +0100

    Updates

--- assets/HomeView-DEh4Hdfj.js
+++
@@ -1,1 +0,0 @@
-import{d as o,_ as s,c as t,a as n,o as a}from"./index-CiIZY0cd.js";const r=o({name:"HomeView",props:{},components:{},setup(){return{}}});function p(c,e,i,m,d,f){return a(),t("div",null,e[0]||(e[0]=[n("h1",{class:"text-red-500"},"This is an home page",-1)]))}const u=s(r,[["render",p]]);export{u as default};
---
+++ assets/HomeView-nVf4O1H4.js
@@ -0,0 +1,1 @@
+import{d as o,_ as s,c as t,a as n,o as a}from"./index-CQoGaxSR.js";const r=o({name:"HomeView",props:{},components:{},setup(){return{}}});function p(c,e,i,m,d,f){return a(),t("div",null,e[0]||(e[0]=[n("h1",{class:"text-red-500"},"This is an home page",-1)]))}const u=s(r,[["render",p]]);export{u as default};
